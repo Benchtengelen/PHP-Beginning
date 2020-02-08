0fbe1723b6fd5ab33296fdf4166e7637c2bd7ea1
Feyyaz Yılmaz <45435495+Benchtengelen@users.noreply.github.com>
2020-02-08 22:55:08 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="tr">
+<head>
+  <meta charset="utf-8">
+  <title></title>
+</head>
+<body>
+ <h1> FİRST COMMAND </h1>
+
+<?php
+ $x = "HUMBLE";
+
+   echo "WORK HARD AND STAY $x MAN";
+
+?>
+
+</body>
+</html>
